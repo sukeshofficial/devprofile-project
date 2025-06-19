8716c96e184d31454b41981691d99146b3f0c2e5
SUKESH D <sukesh.official.2006@gmail.com>
2025-06-19 17:45:37 +0530

    Deleted mahima directory

--- mahima/app/api/endpoints/__init__.py
+++
@@ -1,5 +0,0 @@
-from .readiness import router as readiness_router
-from .job_analyzer import router as job_analyzer_router
-from .profile import router as profile_router
-
-__all__ = ['readiness_router', 'job_analyzer_router', 'profile_router']
--- mahima/app/services/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file makes the services directory a Python package
-from .github_service import GitHubService
-from .job_service import JobService
-from .youtube_service import YouTubeService
-
-__all__ = ['GitHubService', 'JobService', 'YouTubeService']
